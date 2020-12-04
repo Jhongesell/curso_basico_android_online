a7ccb947097ce71b6fd0e70384d9e94cbe3a8e73
Jhon Gesell Villanueva Portella <unmsm182@gmail.com>
2020-12-04 11:32:17 -0500

    Intentando compilar clase Java desde la consola de Android Studio
    Imprime error la consola de Android estudio ya que señala hay un arror con la clase main; parece se debe configurar la 'app', toca investigar como correrlo de forma adecuada para hacer un hello world bien.

--- Book_Paul_Cardle/program02/app/src/main/java/quantum/helloworld/program02/JavaBasics.java
+++ Book_Paul_Cardle/program02/app/src/main/java/quantum/helloworld/program02/JavaBasics.java
@@ -1,7 +1,7 @@
 package quantum.helloworld.program02;
 
 public class JavaBasics {
-    public static void main(String args[]) {
-        System.out.println("Hello world");
+    public static void main(String args[]){
+        System.out.println("Hello World");
     }
 }
